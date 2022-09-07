bccb041d9aee4f4e7145c89c17155c10f4bf7e66
real013228 <real013228@gmail.com>
2022-09-07 22:54:01 +0300

    feat: implement Group class

--- Lab0/Isu/Entities/Group.cs
+++ Lab0/Isu/Entities/Group.cs
@@ -1,3 +1,15 @@
+using Isu.Models;
+using Isu.Services;
+
 namespace Isu.Entities;
 
-public class Group { }
+public class Group
+{
+    private GroupName _nameOfGroup;
+    private List<Student> _students;
+    public Group(GroupName name)
+    {
+        _nameOfGroup = name;
+        _students = new List<Student>();
+    }
+}
