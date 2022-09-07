07973a4f13fc3ff28a64914aaaff482d6a188e0c
real013228 <real013228@gmail.com>
2022-09-07 22:53:46 +0300

    feat: implement GroupName class

--- Lab0/Isu/Models/GroupName.cs
+++ Lab0/Isu/Models/GroupName.cs
@@ -1,3 +1,11 @@
 namespace Isu.Models;
 
-public class GroupName { }
+public class GroupName
+{
+    private string _groupName;
+
+    public GroupName(string groupName)
+    {
+        _groupName = groupName;
+    }
+}
