dbb55437c161e7ccab141c33389e0a36cd63b9fa
real013228 <real013228@gmail.com>
2022-09-07 22:54:16 +0300

    feat: implement CourseNumber class

--- Lab0/Isu/Models/CourseNumber.cs
+++ Lab0/Isu/Models/CourseNumber.cs
@@ -1,3 +1,11 @@
 namespace Isu.Models;
 
-public class CourseNumber { }
+public class CourseNumber
+{
+    private int _courseNumber;
+
+    public CourseNumber(int courseNumber)
+    {
+        _courseNumber = courseNumber;
+    }
+}
