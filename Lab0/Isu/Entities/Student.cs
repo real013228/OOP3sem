d12a769a9188ddcb71e6c66c9de913571120759c
real013228 <real013228@gmail.com>
2022-09-07 22:53:15 +0300

    feat: implement Student class

--- Lab0/Isu/Entities/Student.cs
+++ Lab0/Isu/Entities/Student.cs
@@ -1,3 +1,24 @@
+using Isu.Models;
+
 namespace Isu.Entities;
 
-public class Student { }
+public class Student
+{
+    private int? _id;
+    private string? _name;
+    private CourseNumber? _course;
+
+    public Student()
+    {
+        _id = null;
+        _name = string.Empty;
+        _course = null;
+    }
+
+    public Student(int id, string name, CourseNumber course)
+    {
+        _id = id;
+        _name = name;
+        _course = course;
+    }
+}
