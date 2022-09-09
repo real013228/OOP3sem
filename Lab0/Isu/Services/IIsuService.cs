c70d3346ec29e2d1cabb5a88a8897f0f0020ee13
real013228 <real013228@gmail.com>
2022-09-09 19:15:40 +0300

    feat: implementation of all interface methods

--- Lab0/Isu/Services/IIsuService.cs
+++ Lab0/Isu/Services/IIsuService.cs
@@ -18,3 +18,124 @@ public interface IIsuService
 
     void ChangeStudentGroup(Student student, Group newGroup);
 }
+
+public class IsuService : IIsuService
+{
+    private const int TabelNum = 100000;
+    private List<Group> _groups = new List<Group>();
+    private List<Student> _students = new List<Student>();
+    public Group AddGroup(GroupName name)
+    {
+        var newGroup = new Group(name);
+        _groups.Add(newGroup);
+        return newGroup;
+    }
+
+    public Student AddStudent(Group group, string name)
+    {
+        var newStudent = new Student(TabelNum + StudentsCount(), name, group.NameOfGroup.Course, group.NameOfGroup);
+        group.AddStudent(newStudent);
+        _students.Add(newStudent);
+        return newStudent;
+    }
+
+    public Student GetStudent(int id)
+    {
+        try
+        {
+            return _students[id - TabelNum];
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    public Student? FindStudent(int id)
+    {
+        if (id - TabelNum < StudentsCount())
+        {
+            Student? student = _students[id - TabelNum];
+            return student;
+        }
+
+        return null;
+    }
+
+    public List<Student> FindStudents(GroupName groupName)
+    {
+        var students = new List<Student>();
+        foreach (Group group in _groups)
+        {
+            if (group.NameOfGroup == groupName)
+            {
+                foreach (Student student in group.Students)
+                {
+                    students.Add(student);
+                }
+            }
+        }
+
+        return students;
+    }
+
+    public List<Student> FindStudents(CourseNumber courseNumber)
+    {
+        var students = new List<Student>();
+        foreach (Student student in _students)
+        {
+            if (student.Course == courseNumber)
+            {
+                students.Add(student);
+            }
+        }
+
+        return students;
+    }
+
+    public Group? FindGroup(GroupName groupName)
+    {
+        foreach (Group group in _groups)
+        {
+            if (group.NameOfGroup == groupName)
+            {
+                return group;
+            }
+        }
+
+        return null;
+    }
+
+    public List<Group> FindGroups(CourseNumber courseNumber)
+    {
+        var groups = new List<Group>();
+        foreach (Group group in _groups)
+        {
+            if (group.NameOfGroup.Course == courseNumber)
+            {
+                groups.Add(group);
+            }
+        }
+
+        return groups;
+    }
+
+    public void ChangeStudentGroup(Student student, Group newGroup)
+    {
+        foreach (var group in _groups)
+        {
+            if (group.NameOfGroup == student.NameOfGroup)
+            {
+                group.RemoveStudent(student);
+            }
+        }
+
+        newGroup.AddStudent(student);
+    }
+
+    private int StudentsCount()
+    {
+        return _students.Count;
+    }
+}
