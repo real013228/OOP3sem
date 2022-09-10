8b743197cb5d2b9677b145619c7e91ec8a1f8bf3
real013228 <real013228@gmail.com>
2022-09-10 12:38:46 +0300

    feat: add custom exceptions

---
+++ Lab0/Isu/Services/CreateGroupWithInvalidNameException.cs
@@ -0,0 +1,15 @@
+﻿namespace Isu.Services;
+
+[Serializable]
+public class CreateGroupWithInvalidNameException : Exception
+{
+    public CreateGroupWithInvalidNameException() { }
+
+    public CreateGroupWithInvalidNameException(string message)
+        : base(message)
+    { }
+
+    public CreateGroupWithInvalidNameException(string message, Exception inner)
+        : base(message, inner)
+    { }
+}
---
+++ Lab0/Isu/Services/ReachMaxStudentPerGroupException.cs
@@ -0,0 +1,14 @@
+﻿namespace Isu.Services
+{
+    [Serializable]
+    public class ReachMaxStudentPerGroupException : Exception
+    {
+        public ReachMaxStudentPerGroupException() { }
+
+        public ReachMaxStudentPerGroupException(string message)
+            : base(message) { }
+
+        public ReachMaxStudentPerGroupException(string message, Exception inner)
+            : base(message, inner) { }
+    }
+}
