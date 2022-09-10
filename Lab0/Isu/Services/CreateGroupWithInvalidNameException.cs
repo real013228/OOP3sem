ff0f6c4eb42e8c60699ea98d0f1f7ce409289753
real013228 <real013228@gmail.com>
2022-09-10 12:38:46 +0300

    feat: add custom exceptions

--- Lab0/Isu/Services/CreateGroupWithInvalidNameException.cs
+++ Lab0/Isu/Services/CreateGroupWithInvalidNameException.cs
@@ -6,8 +6,10 @@ public class CreateGroupWithInvalidNameException : Exception
     public CreateGroupWithInvalidNameException() { }
 
     public CreateGroupWithInvalidNameException(string message)
-        : base(message) { }
+        : base(message)
+    { }
 
     public CreateGroupWithInvalidNameException(string message, Exception inner)
-        : base(message, inner) { }
+        : base(message, inner)
+    { }
 }
