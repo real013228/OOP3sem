5fe123cd601c49365f6ec6dc9f7917b4d8a7b074
real013228 <real013228@gmail.com>
2022-09-15 23:08:48 +0300

    add: add InvalidIdValueException exception for validation IdGenerator

---
+++ Lab0/Isu/CustomExceptions/InvalidIdValueException.cs
@@ -0,0 +1,7 @@
+﻿namespace Isu.CustomExceptions;
+
+public class InvalidIdValueException : BaseCustomException
+{
+    public InvalidIdValueException(string value)
+        : base($"Invalid value: {value} should be between 0 and 900000") { }
+}
