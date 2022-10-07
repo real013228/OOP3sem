1a68045930d9ad08fabf20fb48e3fe9f617783ec
real013228 <real013228@gmail.com>
2022-10-07 15:40:40 +0300

    add : some exceptions

---
+++ Lab1/Shops/Exceptions/ShopException.cs
@@ -0,0 +1,15 @@
+﻿using Shops.Entities;
+using Shops.Models;
+
+namespace Shops.Exceptions;
+
+public class ShopException : Exception
+{
+    private ShopException(string msg)
+        : base(msg) { }
+
+    public static ShopException InvalidCountException(Product product)
+    {
+        return new ShopException($"Invalid request: there is no enough count {product.Name}");
+    }
+}
