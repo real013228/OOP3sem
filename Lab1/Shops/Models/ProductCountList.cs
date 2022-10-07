4dced9b27fbf63c81991dba50de86f3247277e66
real013228 <real013228@gmail.com>
2022-10-07 15:40:55 +0300

    add: some models

---
+++ Lab1/Shops/Models/ProductCountList.cs
@@ -0,0 +1,41 @@
+﻿using Shops.Entities;
+using Shops.Exceptions;
+
+namespace Shops.Models;
+
+public class ProductCountList
+{
+    public ProductCountList()
+    {
+        List = new Dictionary<Product, int>();
+    }
+
+    public ProductCountList(Dictionary<Product, int> list)
+    {
+        List = list;
+    }
+
+    public Dictionary<Product, int> List { get; }
+
+    public int GetProductCount(Product product)
+    {
+        if (!List.ContainsKey(product))
+        {
+            throw ProductCountListException.InvalidProductException(product);
+        }
+
+        return List[product];
+    }
+
+    public void AddProduct(Product product, int count)
+    {
+        if (List.ContainsKey(product))
+        {
+            List[product] += count;
+        }
+        else
+        {
+            List.Add(product, count);
+        }
+    }
+}
---
+++ Lab1/Shops/Models/Supply.cs
@@ -0,0 +1,19 @@
+﻿using Shops.Entities;
+using Shops.Exceptions;
+
+namespace Shops.Models;
+
+public class Supply
+{
+    public ProductCountList ProductSupply(Store store, ProductCountList list)
+    {
+        AddProducts(store, list);
+        return list;
+    }
+
+    private ProductCountList AddProducts(Store store, ProductCountList list)
+    {
+        list.List.ToList().ForEach(x => store.List.AddProduct(x.Key, x.Value));
+        return store.List;
+    }
+}
