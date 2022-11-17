9de819223d76bc6d51bba6860f37244079fd6961
real013228 <real013228@gmail.com>
2022-11-17 13:27:14 +0300

    fix: fix

--- Lab3/Backups.Test/Tests/ProgramBackupTest.cs
+++ Lab3/Backups.Test/Tests/ProgramBackupTest.cs
@@ -23,22 +23,4 @@ public static class ProgramBackupTest
         backupTask.AddBackupObject(new BackupObject(@"asasas.txt", repository));
         backupTask.DoJob();
     }
-
-    public static void SerializeTest()
-    {
-        var repository = new Repository(@"C:\Users\real0\OneDrive\real013228\Lab3");
-        var algo = new SplitStorage();
-        var archiver = new ZipArchiver();
-        var backupTask = new BackupTask(new Backup(), repository, algo, archiver, "TaskFinalSplit");
-
-        backupTask.AddBackupObject(new BackupObject(@"Test", repository));
-        backupTask.AddBackupObject(new BackupObject(@"MegaTest2", repository));
-        backupTask.AddBackupObject(new BackupObject(@"asasas.txt", repository));
-        backupTask.DoJob();
-        string output = JsonConvert.SerializeObject(
-            backupTask,
-            Formatting.Indented,
-            new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() });
-        var backupTaskDeserialized = JsonConvert.DeserializeObject(output);
-    }
 }
